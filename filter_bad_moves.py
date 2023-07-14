1a9dd010b47d36d6f1573e3258c39611eba6f1f0
nnourr <nourqwerty123@gmail.com>
2023-07-14 13:14:44 -0400

    fixed enemy collision

--- filter_bad_moves.py
+++ filter_bad_moves.py
@@ -2,21 +2,16 @@
 def filter_self_collision(curr_snake: list, possible_moves: list):
     remove = []
     for move in possible_moves:
-        move = move
         new_position = move['transform'](curr_snake[0])
-        print('new position self collision: ' + str(new_position))
-        print('curr snake: ' + str(curr_snake[:-1]))
         if new_position in curr_snake[:-1]:
             remove.append(move)
 
-    print ("remove self " + str(possible_moves))
     possible_moves = [move for move in possible_moves if move not in remove]
     return possible_moves
 
 def filter_border_collision(curr_snake: list, possible_moves: list, board: dict):
     remove = []
     for move in possible_moves:
-        move = move
         new_position = move['transform'](curr_snake[0])
         if new_position['x'] < 0 \
             or new_position['x'] >= board['width'] \
@@ -28,10 +23,14 @@ def filter_border_collision(curr_snake: list, possible_moves: list, board: dict)
     return possible_moves
 
 def filter_enemy_collision(curr_snake: list, possible_moves: list, enemies: dict):
-    enemy_bodies = [enemy['body'] for enemy in enemies]
+    enemy_bodies = []
+    for enemy in enemies:
+        enemy = enemy['body']
+        for body in enemy:
+            enemy_bodies.append(body)
+    
     remove = []
     for move in possible_moves:
-        move = move
         new_position = move['transform'](curr_snake[0])
         if new_position in enemy_bodies:
             remove.append(move)
